800c9874f4dc3438124394bf4c9e4677232298e4
Ale Zab <galejandroz@hotmail.com>
2019-02-10 21:06:45 -0300

    primera version

---
+++ precache-manifest.8eeb860c966b74538814985afc64b56c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "716707be204d6cc38b2f",
+    "url": "/static/js/main.716707be.chunk.js"
+  },
+  {
+    "revision": "1015926caf23046f2ba3",
+    "url": "/static/js/1.1015926c.chunk.js"
+  },
+  {
+    "revision": "716707be204d6cc38b2f",
+    "url": "/static/css/main.ad08befd.chunk.css"
+  },
+  {
+    "revision": "e4c80b2e666ba65953c9b10d45883b2b",
+    "url": "/index.html"
+  }
+];
